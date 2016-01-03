02d3e0ca6b34b61c198a3c6b50dae30475aad7de
Aceso Aegle <aceso1999@gmail.com>
2016-01-02 16:45:03 -0800

    Okay, here is an example of how to add labels.
    secondNode.addLabel(DynamicLabel.label("test"));

--- src/main/java/org/atgc/bio/util/Neo4JExample.java
+++ src/main/java/org/atgc/bio/util/Neo4JExample.java
@@ -5,10 +5,8 @@
 package org.atgc.bio.util;
 
 import java.net.URISyntaxException;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
+
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.rest.graphdb.RestGraphDatabase;
 import org.neo4j.rest.graphdb.index.RestIndex;
@@ -63,15 +61,31 @@ public class Neo4JExample {
     // Mutating operations go here
             firstNode = graphDb.createNode();
             firstNode.setProperty(key, "prime");
+            /*
+            A label is a grouping facility for Node where all nodes having a label are part of the same group. Labels on nodes are optional
+            and any node can have an arbitrary number of labels attached to it. Objects of classes implementing this interface can be used as
+            label representations in your code. It's very important to note that a label is uniquely identified by its name, not by any particular
+            instance that implements this interface. This means that the proper way to check if two labels are equal is by invoking equals() on
+            their names, NOT by using Java's identity operator (==) or equals() on the Label instances. A consequence of this is that you can
+            NOT use Label instances in hashed collections such as HashMap and HashSet. However, you usually want to check whether a specific node
+            instance has a certain label. That is best achieved with the Node.hasLabel(Label) method. For labels that your application know up
+            front you should specify using an enum, and since the name is accessed using the name() method it fits nicely. public enum MyLabels
+            implements Label { PERSON, RESTAURANT; } For labels that your application don't know up front you can make use of
+            DynamicLabel.label(String), or your own implementation of this interface, as it's just the name that matters.
+             */
+            firstNode.addLabel(DynamicLabel.label("test"));
 
             secondNode = graphDb.createNode();
             secondNode.setProperty(key, "galaxy" );
+            secondNode.addLabel(DynamicLabel.label("test"));
+
             relationship = firstNode.createRelationshipTo(secondNode, RelTypes.INHIBITS );
             relationship.setProperty( "message", "brave heart Neo4j " );
 
             myIndex = graphDb.index().forNodes(indexName);
             myIndex.add(firstNode, key, "prime");
             myIndex.add(secondNode, key, "galaxy");
+
             tx.success();
             
         } finally {
