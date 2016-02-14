9d7cb4ac0ec31ca3f5cb20d4342ce7b6c4ba0117
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2016-02-13 21:11:15 -0800

    cleaned up the code

--- src/main/java/org/atgc/bio/util/EnzymeCitationImport.java
+++ src/main/java/org/atgc/bio/util/EnzymeCitationImport.java
@@ -2,6 +2,7 @@ package org.atgc.bio.util;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCursor;
+import org.apache.http.HttpException;
 import org.apache.logging.log4j.Logger;
 import org.atgc.bio.*;
 import org.atgc.bio.domain.*;
@@ -9,7 +10,10 @@ import org.atgc.bio.repository.PersistenceTemplate;
 import org.atgc.bio.repository.Subgraph;
 import org.atgc.mongod.MongoCollection;
 import org.atgc.mongod.MongoUtil;
+
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.util.*;
 import org.apache.logging.log4j.LogManager;
@@ -30,6 +34,7 @@ import org.neo4j.graphdb.NotFoundException;
  * 
  * @author jtanisha-ee
  */
+@SuppressWarnings("javadoc")
 public class EnzymeCitationImport {
     
     protected static Logger log = LogManager.getLogger(EnzymeCitationImport.class);
@@ -39,15 +44,14 @@ public class EnzymeCitationImport {
         return mongoUtil.getCollection(coll.toString());
     }
      
-    public static void main(String[] args) throws java.io.IOException, UnknownHostException, Exception {
-        DBCursor dbCursor = getCollection(ImportCollectionNames.ENZYME).findDBCursor("{}" );
-       
-        try {
+    public static void main(String[] args) throws UnknownHostException {
+
+        try (DBCursor dbCursor = getCollection(ImportCollectionNames.ENZYME).findDBCursor("{}")) {
             // we expect only one document match
             while (dbCursor.hasNext()) {
-                BasicDBObject result = (BasicDBObject)dbCursor.next();
+                BasicDBObject result = (BasicDBObject) dbCursor.next();
                 log.info("result =" + result.toString());
-                String citeKey = (String)result.get(EnzymeFields.CITE_KEY.toString());
+                String citeKey = (String) result.get(EnzymeFields.CITE_KEY.toString());
                 log.info("###### citeKey =" + citeKey);
                 // until then a quick fix. later remove it.
                 try {
@@ -58,28 +62,27 @@ public class EnzymeCitationImport {
                 log.info("ADDED NEW PROPERTIES: " + PersistenceTemplate.getPropertyCount() + ", SET PROPERTIES: " + PersistenceTemplate.getPropertySetCount() + ", ADDED NEW NODES: " + PersistenceTemplate.getIndexNodeCount());
                 log.info("ADDED NEW PROPERTIES BY INDEX: " + PersistenceTemplate.getPropertyCounts() + ", SET PROPERTIES BY INDEX: " + PersistenceTemplate.getPropertySetCounts() + ", ADDED NEW NODES BY INDEX: " + PersistenceTemplate.getIndexNodeCounts());
             }
-        } finally {
-           dbCursor.close();
         }
         log.info("ADDED NEW PROPERTIES: " + PersistenceTemplate.getPropertyCount() + ", SET PROPERTIES: " + PersistenceTemplate.getPropertySetCount() + ", ADDED NEW NODES: " + PersistenceTemplate.getIndexNodeCount());
         log.info("ADDED NEW PROPERTIES BY INDEX: " + PersistenceTemplate.getPropertyCounts() + ", SET PROPERTIES BY INDEX: " + PersistenceTemplate.getPropertySetCounts() + ", ADDED NEW NODES BY INDEX: " + PersistenceTemplate.getIndexNodeCounts());
         log.info("Completed successfully!");
     }
-    
+
+    /*
     public static boolean isObj(BasicDBObject obj) {
            if (obj.get(EnzymeFields.TEXT.toString()) != null) {
               // log.info(field.toString() + " value exists");
               return true;
            }
         return false;
-    }
+    }*/
     
     public static String getValue(BasicDBObject obj) {
         return obj.getString(EnzymeFields.TEXT.toString());
     }
     
     
-    public static Enzyme getEnzyme(String ecNum, Subgraph subGraph) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, NotFoundException, InvocationTargetException, UnknownHostException, RuntimeException, Exception {
+    public static Enzyme getEnzyme(String ecNum, Subgraph subGraph) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, NotFoundException, InvocationTargetException, UnknownHostException {
         Enzyme enzyme = (Enzyme)subGraph.search(BioTypes.ENZYME, BioFields.ENZYME_ID, ecNum);
         if (enzyme == null) {
             enzyme = EnzymeImport.getEnzyme(ecNum, subGraph);
@@ -92,27 +95,24 @@ public class EnzymeCitationImport {
     }
     
     
-    public static void processEnzymeCiteKey(String citeKey) throws UnknownHostException, RuntimeException, Exception {
+    public static void processEnzymeCiteKey(String citeKey) throws IOException, RuntimeException, URISyntaxException, HttpException, NoSuchFieldException, IllegalAccessException, InvocationTargetException, InterruptedException {
             BasicDBObject basicDBObject = new BasicDBObject();
             basicDBObject.put(EnzymeCitationFields.CITE_KEY.toString(), citeKey);
-            DBCursor dbCursor = getCollection(ImportCollectionNames.ENZYME_CITATION).findDBCursor(basicDBObject);
-            try {
-                if (dbCursor.hasNext()) {
-                    BasicDBObject result = (BasicDBObject)dbCursor.next();
-                    log.info("map" + result.toString());
-                    log.info("citeKey =" + result.get(EnzymeCitationFields.CITE_KEY.toString()));
-                    log.info("pubMedId =" + result.get(EnzymeCitationFields.PUBMED_ID.toString()));
-                    //log.info("row =" + map.get(EnzymeFields.ROW.toString()));
-                    String pubMedId = getPubMedId(result);
-                    if (pubMedId != null) { 
-                        Subgraph subGraph = new Subgraph();
-                        createCitationRelations(citeKey, pubMedId, subGraph);
-                        PersistenceTemplate.saveSubgraph(subGraph);
-                    }
+        try (DBCursor dbCursor = getCollection(ImportCollectionNames.ENZYME_CITATION).findDBCursor(basicDBObject)) {
+            if (dbCursor.hasNext()) {
+                BasicDBObject result = (BasicDBObject) dbCursor.next();
+                log.info("map" + result.toString());
+                log.info("citeKey =" + result.get(EnzymeCitationFields.CITE_KEY.toString()));
+                log.info("pubMedId =" + result.get(EnzymeCitationFields.PUBMED_ID.toString()));
+                //log.info("row =" + map.get(EnzymeFields.ROW.toString()));
+                String pubMedId = getPubMedId(result);
+                if (pubMedId != null) {
+                    Subgraph subGraph = new Subgraph();
+                    createCitationRelations(citeKey, pubMedId, subGraph);
+                    PersistenceTemplate.saveSubgraph(subGraph);
                 }
-            } finally {
-                dbCursor.close();
             }
+        }
     }   
     
     
@@ -124,17 +124,17 @@ public class EnzymeCitationImport {
      * @throws RuntimeException
      * @throws Exception 
      */
-    public static List<Map> getEnzymeIdList(String citeKey) throws UnknownHostException, RuntimeException, Exception {
+    public static List<Map> getEnzymeIdList(String citeKey) throws UnknownHostException, RuntimeException {
         return EnzymeCiteKeyUtil.getEcNumObjects(citeKey);
         
     }
     
-    public static void createPubMedRelation(PubMed pubMed, Enzyme enzyme) throws Exception {
+    public static void createPubMedRelation(PubMed pubMed, Enzyme enzyme)  {
         log.info("createPubMedRelation");
           pubMed.setPubMedRelation(enzyme);
     }
     
-    public static void createCitationRelations(String citeKey, String pubMedId, Subgraph subGraph) throws UnknownHostException, RuntimeException, Exception {  
+    public static void createCitationRelations(String citeKey, String pubMedId, Subgraph subGraph) throws IOException, RuntimeException, IllegalAccessException, InterruptedException, HttpException, URISyntaxException, InvocationTargetException, NoSuchFieldException {
         /**
          * Returns ec_num (enzyme numbers) from enzymecitekey collection
          */
@@ -156,10 +156,19 @@ public class EnzymeCitationImport {
     
     /**
      * createEnzymePubMedRelation
-     * @param pubMed  {@link BioTypes#PubMed}
-     * @param ecNumMaps - enzymeNumber  
+     * @param pubMed
+     * @param ecNumMaps - enzymeNumber
+     * @param subGraph
+     * @throws NoSuchFieldException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws NotFoundException
+     * @throws InvocationTargetException
+     * @throws UnknownHostException
+     * @throws RuntimeException
+     * @throws Exception
      */
-    public static void createEnzymePubMedRelation(PubMed pubMed, List<Map> ecNumMaps, Subgraph subGraph) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, NotFoundException, InvocationTargetException, UnknownHostException, RuntimeException, Exception {
+    public static void createEnzymePubMedRelation(PubMed pubMed, List<Map> ecNumMaps, Subgraph subGraph) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, NotFoundException, InvocationTargetException, UnknownHostException {
         for (Map map : ecNumMaps) {
             if (map.size() > 0) {
                 String ecNum = (String)map.get(EnzymeFields.EC_NUM.toString());
@@ -185,7 +194,7 @@ public class EnzymeCitationImport {
      * @throws IllegalAccessException
      * @throws Exception 
      */
-    public static PubMed getPubMed(String pubMedId, Subgraph subGraph) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, Exception {
+    public static PubMed getPubMed(String pubMedId, Subgraph subGraph) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, URISyntaxException, IOException, HttpException, InterruptedException, InvocationTargetException {
           PubMed pubMed = (PubMed)subGraph.search(BioTypes.PUBMED, BioFields.PUBMED_ID, pubMedId);    
           if (pubMed == null) {
              pubMed = PubMedUtil.getPubmed(pubMedId, subGraph);
