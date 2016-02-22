f41cb5eba25c552c6ae741d584e0b3f328919450
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2016-02-21 19:50:11 -0800

    removed public

--- src/main/java/org/atgc/bio/meta/Indexed.java
+++ src/main/java/org/atgc/bio/meta/Indexed.java
@@ -16,5 +16,5 @@ import java.lang.annotation.RetentionPolicy;
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Indexed {
-    public IndexNames indexName();
+    IndexNames indexName();
 }
