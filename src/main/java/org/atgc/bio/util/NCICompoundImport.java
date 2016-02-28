ae7454cd8802fc8f3da163784166d537bace486c
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2016-02-28 12:19:55 -0800

    removed dependnecy on compound list

--- src/main/java/org/atgc/bio/util/NCICompoundImport.java
+++ src/main/java/org/atgc/bio/util/NCICompoundImport.java
@@ -3,8 +3,12 @@
  * and open the template in the editor.
  */
 package org.atgc.bio.util;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.atgc.bio.NCICompoundUtil;
+import org.atgc.fileio.FileWrite;
 
+import java.io.File;
 import java.net.UnknownHostException;
 
 /**
@@ -15,6 +19,8 @@ public class NCICompoundImport {
     
     final static int MAX_INDEX = 6303; // currently max urls on hotdiary.com
     final static int DOC_INDEX = 3;
+    private static String DIR_PATH = "/usr/local/redbasin/data/ncicompound/compound/";
+    protected static Logger log = LogManager.getLogger(NCICompoundImport.class);
    
     
      /**
@@ -22,16 +28,27 @@ public class NCICompoundImport {
      * importstatus - due is by default.
      * @@throws UnknownHostException
      */
-    private static void addCompoundList() throws UnknownHostException {
+    /*private static void addCompoundList() throws UnknownHostException {
          NCICompoundUtil.addCompoundList();
-    }
-    
-    
+    }*/
+
     public static void main(String[] args) throws java.io.IOException {
-        for (int i = 1; i <= MAX_INDEX; i++) {           
-            String url = "http://hotdiary.com/nci/compound/compound" + i + ".xml";
+
+        File dir = new File(DIR_PATH);
+        //File[] files = dir.listFiles(new ExtFilter("xml"));
+        File[] files = dir.listFiles();
+
+        //addCompoundList();
+
+        for (int i = 1; i <= MAX_INDEX; i++) {
+            String fileName = files[i].getName();
+            log.info("fileName =" + fileName);
+            String compoundName = fileName.substring(0, fileName.indexOf(".xml"));
+            String s = FileWrite.readFile(DIR_PATH + fileName);
+            //String url = "http://hotdiary.com/nci/compound/compound" + i + ".xml";
             //System.out.println( "i = " + i  + " url =" + url);
-            NCICompoundUtil.addCompound(url);
+            NCICompoundUtil.addCompoundName(s);
+            //NCICompoundUtil.addCompound(url);
         }     
         
     }
